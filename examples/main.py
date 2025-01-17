f2a546208d6c174f01032f710cebe81fc1a489a5
XenonPy <xenonpy@wearehackerone.com>
2025-01-16 18:01:37 -0800

    Add examples, first publish

---
+++ examples/main.py
@@ -0,0 +1,16 @@
+from steno import EncodeInImage, DecodeFromImage
+
+path = input("Enter the path of the image file: ")
+message = input("Enter the message to encode: ")
+password = input("Enter the password (optional): ")
+print("Encoding message...")
+if password:
+    EncodeInImage(path, message, password)
+else:
+    EncodeInImage(path, message)
+print("Message encoded successfully!")
+password2 = input("Enter the password to decode the message (optional): ")
+print("\n")
+input("Press Enter to decode the message...")
+decoded_message = DecodeFromImage(f"{path.split(".")[0] + "_encoded" + path.split(".")[1]}", password2)
+print(decoded_message)
